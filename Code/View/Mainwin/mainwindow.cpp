f5646076158f07db935c91eb935aab771917fa22
ZefinNg <wuzefeng517@163.com>
2020-03-06 23:55:11 +0800

    添加菜单栏

--- Code/View/Mainwin/mainwindow.cpp
+++ Code/View/Mainwin/mainwindow.cpp
@@ -1,15 +1,49 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#define APP_WIDTH  (1280)
+#define APP_HEIGHT (720)
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     this->setWindowIcon(QIcon(":/Logo.ico"));
+    this->setWindowTitle(tr("CameraController"));
+    this->resize(APP_WIDTH, APP_HEIGHT);
+
+    QDesktopWidget *desktopWidget = QApplication::desktop();
+    this->move((desktopWidget->width() - this->width()) / 2,
+               (desktopWidget->height() - this->height()) / 2);
+
+    m_settingMenu = this->menuBar()->addMenu(tr("相机配置"));
+    m_aboutMenu   = this->menuBar()->addMenu(tr("关于"));
+
+    m_fullScreenAction  = new QAction(tr("全屏"), m_settingMenu);
+    m_fullScreenAction->setCheckable(true);
+    m_firstChoiceAction = new QAction(tr("首选项"), m_settingMenu);
+
+    m_settingMenu->addAction(m_fullScreenAction);
+    m_settingMenu->addAction(m_firstChoiceAction);
+
+    connect(m_fullScreenAction,  SIGNAL(triggered(bool)), this, SLOT(onFullScreenAction(bool)));
+    connect(m_firstChoiceAction, SIGNAL(triggered()),     this, SLOT(onFirstChoiceAction()));
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::onFullScreenAction(bool checked)
+{
+
+}
+
+void MainWindow::onFirstChoiceAction()
+{
+
 }
--- Code/View/Mainwin/mainwindow.h
+++ Code/View/Mainwin/mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 #include <QIcon>
+#include <QMenu>
+#include <QDesktopWidget>
+#include <QAction>
+#include <QDebug>
 
 namespace Ui {
 class MainWindow;
@@ -16,8 +20,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void onFullScreenAction();
+    void onFirstChoiceAction();
+
 private:
     Ui::MainWindow *ui;
+    QMenu *m_settingMenu;
+    QAction *m_fullScreenAction;
+    QAction *m_firstChoiceAction;
+
+    QMenu *m_aboutMenu;
 };
 
 #endif // MAINWINDOW_H
